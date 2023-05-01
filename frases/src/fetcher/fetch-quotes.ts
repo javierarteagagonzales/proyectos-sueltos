ed00fdf17604281fc03979f68ada185d79725ee0
Javier <javalejan12@gmail.com>
2023-05-01 03:08:01 -0500

    proyectos frases

---
+++ frases/src/fetcher/fetch-quotes.ts
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+interface ParseDataParams {
+  en: string;
+  author: string;
+}
+
+interface ParseDataReturn {
+  quote: string;
+  author: string;
+}
+
+const parseData = (data: ParseDataParams) => {
+  return {
+    quote: data.en,
+    author: data.author
+  };
+};
+
+// Recursively find a random quote of correct length.
+const randomQuote = (data: ParseDataParams[]): ParseDataParams => {
+  let randQuote = data[Math.floor(Math.random() * data.length)];
+  return randQuote.en.length < 220 ? randQuote : randomQuote(data);
+};
+
+export async function fetchQuotes(): Promise<ParseDataReturn> {
+  // NOTE: Heroku has no more free tier. Time for new API?
+  // const response = await axios.get(
+  //   'https://programming-quotes-api.herokuapp.com/Quotes/random'
+  // );
+  // Well it did stop haha.
+
+  const response = await axios.get(
+    'https://github.com/javierarteagagonzales/proyectos-sueltos/blob/main/frases.json'
+  );
+
+  const data = response.data;
+
+  // Get a random Quote.
+  let randQuote = randomQuote(data);
+
+  // Parse the data and return it.
+  return parseData(randQuote);
+}
